8e682d9bbe77ca375810e5f176b137eb505e3f00
prakhar362 <prakharshri2005@gmail.com>
2025-06-16 21:43:36 +0530

    grid trial pahse 1

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -2,19 +2,50 @@ import { cn } from "@/lib/utils";
 import React from "react";
 import { BentoGrid, BentoGridItem } from "./ui/bento-grid";
 import Image from "next/image";
+import { World } from "./ui/globe";
+
+const globeConfig = {
+  pointSize: 1,
+  atmosphereColor: "#ffffff",
+  showAtmosphere: true,
+  atmosphereAltitude: 0.1,
+  polygonColor: "rgba(255,255,255,0.7)",
+  globeColor: "#1d072e",
+  emissive: "#000000",
+  emissiveIntensity: 0.1,
+  shininess: 0.9,
+  arcTime: 2000,
+  arcLength: 0.9,
+  rings: 1,
+  maxRings: 3,
+  ambientLight: "#ffffff",
+  directionalLeftLight: "#ffffff",
+  directionalTopLight: "#ffffff",
+  pointLight: "#ffffff",
+};
+
+const globeData = [
+  {
+    order: 1,
+    startLat: 48.8566,
+    startLng: 2.3522,
+    endLat: 40.7128,
+    endLng: -74.0060,
+    arcAlt: 0.5,
+    color: "#ffffff"
+  }
+];
 
 const items = [
   {
     id: 1,
-     title: "I prioritize client collaboration, fostering open communication ",
+    title: "I prioritize client collaboration, fostering open communication ",
     description: "",
     className: "md:col-span-2",
     imgClassName: "absolute right-0 -bottom-20 md:w-96 w-60",
     titleClassName: "justify-end",
     img: "/b1.svg",
     spareImg: "",
-    
-    
   },
   {
     id: 2,
@@ -25,30 +56,27 @@ const items = [
     titleClassName: "justify-start",
     img: "",
     spareImg: "",
-   
-    
   },
   {
     id: 3,
-    title: "Do you want to start a project together?",
-    description: "",
-    className: "md:col-span-1",
-    imgClassName: "",
-    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
-    img: "",
-    spareImg: "",
-  },
-  {
-    id: 4,
     title: "Currently building a JS Animation library",
     description: "The Inside Scoop",
-    className: "md:col-span-2",
+    className: "md:col-span-1",
     imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
     titleClassName: "justify-center md:justify-start lg:justify-center",
     img: "/b5.svg",
     spareImg: "/grid.svg",
   },
-  
+  {
+    id: 4,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "md:col-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  }
 ];
 
 const Grid = () => {
@@ -58,29 +86,14 @@ const Grid = () => {
         {items.map((item, i) => (
           <BentoGridItem
             key={i}
+            id={item.id}
             title={item.title}
             description={item.description}
             className={item.className}
-            header={
-              <div className="relative w-full h-full">
-                {item.img && (
-                  <Image
-                    src={item.img}
-                    alt={item.title}
-                    fill
-                    className={cn("object-cover rounded-xl", item.imgClassName)}
-                  />
-                )}
-                {item.spareImg && (
-                  <Image
-                    src={item.spareImg}
-                    alt="spare"
-                    fill
-                    className="object-cover rounded-xl"
-                  />
-                )}
-              </div>
-            }
+            img={item.img}
+            imgClassName={item.imgClassName}
+            titleClassName={item.titleClassName}
+            spareImg={item.spareImg}
           />
         ))}
       </BentoGrid>
