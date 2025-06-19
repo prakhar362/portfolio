f51a07db2ebfd7eb9a3800867dfa5e237dd54b96
prakhar362 <prakharshri2005@gmail.com>
2025-06-19 11:39:27 +0530

    trial 6

--- components/ui/background-beams-with-collision.tsx
+++ components/ui/background-beams-with-collision.tsx
@@ -180,15 +180,15 @@ const CollisionMechanism = React.forwardRef<
         ref={beamRef}
         animate="animate"
         initial={{
-          translateY: beamOptions.initialY || "-200px",
-          translateX: beamOptions.initialX || "0px",
-          rotate: beamOptions.rotate || 0,
+          y: beamOptions.initialY ?? -200,
+  x: beamOptions.initialX ?? 0,
+  rotate: beamOptions.rotate ?? 0,
         }}
         variants={{
           animate: {
-            translateY: beamOptions.translateY || "1800px",
-            translateX: beamOptions.translateX || "0px",
-            rotate: beamOptions.rotate || 0,
+            y: beamOptions.translateY ?? 1800,
+  x: beamOptions.translateX ?? 0,
+  rotate: beamOptions.rotate ?? 0,
           },
         }}
         transition={{
