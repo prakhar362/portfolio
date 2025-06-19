fe9e23a4371bb63f429f00ea1f9fedd842b8fe28
prakhar362 <prakharshri2005@gmail.com>
2025-06-19 11:44:34 +0530

    trial7

--- components/ui/globe.tsx
+++ components/ui/globe.tsx
@@ -2,6 +2,7 @@
 import { useEffect, useRef, useState } from "react";
 import { Color, Scene, Fog, PerspectiveCamera, Vector3 } from "three";
 import ThreeGlobe from "three-globe";
+import * as THREE from "three";
 import { useThree, Canvas, extend } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
 import countries from "@/data/globe.json";
@@ -62,7 +63,7 @@ interface WorldProps {
 
 export function Globe({ globeConfig, data }: WorldProps) {
   const globeRef = useRef<ThreeGlobe | null>(null);
-  const groupRef = useRef();
+    const groupRef = useRef<THREE.Group | null>(null); // ✅ fix
   const [isInitialized, setIsInitialized] = useState(false);
 
   const defaultProps = {
@@ -162,7 +163,7 @@ export function Globe({ globeConfig, data }: WorldProps) {
       .arcStartLng((d) => (d as { startLng: number }).startLng * 1)
       .arcEndLat((d) => (d as { endLat: number }).endLat * 1)
       .arcEndLng((d) => (d as { endLng: number }).endLng * 1)
-      .arcColor((e) => (e as { color: string }).color)
+      .arcColor((e:any) => (e as { color: string }).color)
       .arcAltitude((e) => (e as { arcAlt: number }).arcAlt * 1)
       .arcStroke(() => [0.32, 0.28, 0.3][Math.round(Math.random() * 2)])
       .arcDashLength(defaultProps.arcLength)
@@ -231,7 +232,7 @@ export function Globe({ globeConfig, data }: WorldProps) {
   return <group ref={groupRef} />;
 }
 
-export function WebGLRendererConfig() {
+export function WebGLRendererConfig() :any{
   const { gl, size } = useThree();
 
   useEffect(() => {
